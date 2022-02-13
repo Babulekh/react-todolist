b057c1c9b171735f05fa9141b2e65524c6e1b3a5
Ivan <vanca624@gmail.com>
2022-02-13 23:21:11 +0300

    Made app frame

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import react from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+function TodoName() {
+    return <input className="todoName todoControl" type="text" name="todoName" />;
+}
+
+function TodoText() {
+    return <input className="todoText todoControl" type="text" name="todoText" />;
+}
+
+function TodoAddBtn() {
+    return <input className="todoAddBtn todoControl" type="button" name="todoAddBtn" value="Add todo" />;
+}
+
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todoList: [{ Name: "Name", Text: "Text" }],
+        };
+    }
+
+    renderTodoList() {
+        let todoList = [];
+
+        for (let i = 0; i < this.state.todoList.length; i++) {
+            todoList.push(
+                <div className="todo" key={i}>
+                    <p className="todoName">{this.state.todoList[i].Name}</p>
+                    <p className="todoText">{this.state.todoList[i].Text}</p>
+                </div>
+            );
+        }
+
+        return todoList;
+    }
+
+    render() {
+        this.state.todoList.push({ Name: "Name1", Text: "Text1" });
+
+        return this.renderTodoList();
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <div className="App">
+        <div className="todoControls">
+            <TodoName />
+            <TodoText />
+            <TodoAddBtn />
+        </div>
+        <div className="todoList">
+            <TodoList />
+        </div>
+    </div>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
